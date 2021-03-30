97866f3d89ae13c6c22977aacc4202f3e1bef66a
Stephen Seagrave <sseagrave@oneviewhealthcare.com>
2021-03-30 15:11:23 +0100

    Add project files.

---
+++ MyClassesTest/FileProcessTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyClasses;
+
+namespace MyClassesTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"c:/windows/regedit.exe");
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(@"c:/windows/bogus.exe");
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_UsingAttribute()
+        {
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists(""); 
+        }
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_UsingTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+
+            try
+            {
+                fp.FileExists("file");
+            }
+            catch (ArgumentException)
+            {
+                return;
+            }
+            Assert.Fail("Call to FileExists() did not thrown an ArgumentNullException");
+        }
+    }
+}
