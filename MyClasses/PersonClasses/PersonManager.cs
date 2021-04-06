f58a0e9bfffb2e446482efce627ebba8016e3d81
nemixu <s.r.seagrave@gmail.com>
2021-04-06 10:50:44 +0100

    Added person classes to check specific object types

---
+++ MyClasses/PersonClasses/PersonManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;  
+
+namespace MyClasses
+{
+    public class PersonManager
+    {
+        public Person CreatePerson(string first,
+                                   string last,
+                                   bool isSupervisor)
+        {
+            Person ret = null;
+
+            if (!string.IsNullOrEmpty(first))
+            {
+                if (isSupervisor)
+                {
+                    ret = new Supervisor();
+                }
+                else { ret = new Employee();
+                }
+
+                // Assign Variables
+                ret.FirstName = first;
+                ret.LastName = last;
+            }
+            return ret;
+        }
+
+        public List<Person> GetPeople()
+        {
+            List<Person> people = new List<Person>();
+
+            people.Add(new Person() { FirstName = "Paul", LastName = "Doe", });
+            people.Add(new Person() { FirstName = "John", LastName = "Dope", });
+            people.Add(new Person() { FirstName = "Hairy", LastName = "Jim", });
+
+            return people;
+        }
+    }
+}
